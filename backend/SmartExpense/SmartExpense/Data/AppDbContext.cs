ad974a65570d788a77453b9c3a3c3a0c1b1250a5
umesha <umemahee@gmai.com>
2025-11-25 08:54:15 +0530

    Initial Release With the Project Structure

---
+++ backend/SmartExpense/SmartExpense/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SmartExpense.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        // Entities will be added here later:
+        // public DbSet<Expense> Expenses { get; set; }
+        // public DbSet<Category> Categories { get; set; }
+    }
+}
